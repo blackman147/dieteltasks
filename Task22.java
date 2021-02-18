82a23d34f065e7348c469246fe7d0c53f001a6cb
Francis Ntoka <chuksntoka@gmail.com>
2021-02-18 04:44:48 +0100

    new task for chapter 2

---
+++ Task22.java
@@ -0,0 +1,10 @@
+public class Task22{
+
+	public static void main(String[] args){
+
+	System.out.printf("* * * * * * * * %n * * * * * * * * %n* * * * * * * * %n * * * * * * * * %n* * * * * * * * %n * * * * * * * * %n* * * * * * * * %n * * * * * * * *" );
+
+
+}
+
+}
