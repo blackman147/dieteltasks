c16750b57171d298a9f7dada40977cee05c015dd
blackman147 <chuksntoka@gmail.com>
2021-02-28 23:43:47 +0100

    assignment

---
+++ chapterFour/AverageGradeFor.java
@@ -0,0 +1,31 @@
+package chapterFour;
+
+import java.util.Scanner;
+
+public class AverageGradeFor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double result = 0;
+        int count = 0;
+
+        for( count=0; count < 10; count++){
+            System.out.println("Enter students score: ");
+            double score = input.nextDouble();
+            if (score >= 1 && score <= 100) {
+                result += score;
+            }
+            else
+            if(score != count){
+
+                System.out.println("this is an in valid input");
+            }
+
+
+        }
+
+        double average = result/ (count)*1.0;
+        System.out.print("Average equals: " + average);
+    }
+
+
+}
