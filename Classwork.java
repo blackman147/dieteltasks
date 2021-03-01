a4b661ae606fd4a57d5240b9debe37b177e86fa6
blackman147 <chuksntoka@gmail.com>
2021-03-01 07:08:09 +0100

    updated

---
+++ Classwork.java
@@ -0,0 +1,23 @@
+
+public class Classwork {
+
+    public static void main(String[] args) {
+        int i;
+        int counter = 1;
+
+        for (i = 7; i <= 77; i+=7) {
+            counter++;
+            System.out.print(" " + counter++);
+            System.out.println();
+            System.out.print(" " + i);
+        }
+    }
+}
+
+//
+//            System.out.printf(" " + i * i * i  );
+//
+//
+//            }
+
+
