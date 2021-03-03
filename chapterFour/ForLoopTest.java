5205c507076e04c0f04ca7666cd5a230da1d4888
blackman147 <chuksntoka@gmail.com>
2021-03-03 21:18:42 +0100

    invoice

---
+++ chapterFour/ForLoopTest.java
@@ -0,0 +1,17 @@
+package chapterFour;
+
+public class ForLoopTest {
+    public static void main(String[] args) {
+        for (int counter = 1; counter<=10; counter++)
+            if (counter == 5) {
+
+            }
+            else {
+
+            System.out.printf("%d ", counter);
+            }
+
+    }
+
+}
+
