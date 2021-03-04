519e1e6f45d70386abb36b5454685e36113d82ff
blackman147 <chuksntoka@gmail.com>
2021-03-04 06:22:24 +0100

    updated

---
+++ test/chapterThree/modifiedAccountTest.java
@@ -0,0 +1,25 @@
+package chapterThree;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class modifiedAccountTest {
+
+    @Test
+    void getAccountName() {
+
+    }
+
+    @Test
+    void setAccountName() {
+    }
+
+    @Test
+    void getAccountBalance() {
+    }
+
+    @Test
+    void setAccountBalance() {
+    }
+}
