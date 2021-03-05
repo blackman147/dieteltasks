6c9eff3604932a2beff3ebfd29252e4e7f517542
blackman147 <chuksntoka@gmail.com>
2021-03-05 10:23:14 +0100

    chapter two exercise

---
+++ chapterThree/chapterThree/SepartingTheDigits0fAninteger.java
@@ -0,0 +1,34 @@
+package chapterThree;
+
+import java.util.Scanner;
+
+public class SepartingTheDigits0fAninteger {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a five digit Number: ");
+
+        int number = scan.nextInt();
+
+        int firstDigit = number/10000;
+
+        int secondNumber = number % 10000;
+
+        int secondDigit = secondNumber/1000;
+
+        int  thirdNumber = number % 1000;
+
+        int thirdDigit = thirdNumber/ 100;
+
+        int fourthNumber = number % 100;
+
+        int fourthDigit = fourthNumber / 10;
+
+        int fifthDigit = fourthNumber % 10;
+
+        System.out.println(firstDigit + " " + secondDigit + " " + thirdDigit + " " + fourthDigit + " " + fifthDigit + " ");
+
+
+
+
+    }
+}
