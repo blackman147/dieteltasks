9c5d0cb599d5c214740af7304d3132de953a1062
blackman147 <chuksntoka@gmail.com>
2021-05-21 17:29:40 +0100

    Exercises

--- test/ChapterSevenTest/PlayerTest.java
+++ test/ChapterSevenTest/PlayerTest.java
@@ -1,2 +1,26 @@
-package ChapterSevenTest;public class PlayerTest {
+package ChapterSevenTest;
+
+import org.junit.jupiter.api.Test;
+
+import static ChapterSevenTest.Suit.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlayerTest {
+
+    @Test
+    void playerHasAnId(){
+        Player player = new Player("Blackie");
+        assertEquals("Blackie", player.getName());
+    }
+
+    @Test
+    void playerHasCards(){
+        Player player = new Player("Blackie");
+        player.addCard(new Card(DIAMOND, 4));
+        player.addCard(new Card(SPADES, 2));
+        player.addCard(new Card(HEARTS, 7));
+
+        assertEquals(3, player.getNumberOfCards());
+
+    }
 }
