67b5771e19ae722c483d0501a0610a2cca880dfd
blackman147 <chuksntoka@gmail.com>
2021-05-21 15:05:22 +0100

    Exercise

--- test/KataTest/KataTest.java
+++ test/KataTest/KataTest.java
@@ -1,2 +1,35 @@
-package KataTest;public class KataTest {
+package KataTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class KataTest {
+    @Test
+    void numbersCanBeShuffled(){
+        Kata2 kata = new Kata2();
+        int[] numbers = {2,0,1,5};
+        assertEquals(5, numbers[3]);
+        kata.shuffle(numbers);
+        assertNotEquals(2, numbers[0]);
+    }
+
+    @Test
+    void numbersInArrayCanBeSortedInAscendingOrder(){
+        Kata2 kata = new Kata2();
+        int[] numbers = {2,0,1,5};
+        assertEquals(5, numbers[3]);
+        kata.sortAscendingOrder(numbers);
+        assertEquals(1, numbers[1]);
+    }
+
+    @Test
+    void numbersInArrayCanBeSortedInDescendingOrder(){
+        Kata2 kata = new Kata2();
+        int[] numbers = {2,0,1,5};
+        assertEquals(5, numbers[3]);
+        kata.sortDescendingOrder(numbers);
+        assertEquals(1, numbers[2]);
+    }
+
 }
